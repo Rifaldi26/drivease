cfa76d716e1ef5df8bfc4d9ca63374d3b01ad34b
paii26_ <mr015091@gmail.com>
2026-06-08 09:21:20 +0700

    fix ngrok

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,11 +5,17 @@ namespace App\Providers;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        // Memaksa Laravel menggunakan HTTPS
+        if (config('app.env') !== 'local' || request()->server('HTTP_X_FORWARDED_PROTO') == 'https') {
+            URL::forceScheme('https');
+        }
+
         // Custom tampilan email verifikasi
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
             return (new MailMessage)
